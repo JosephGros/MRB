768ac835da183606e64341c16c3798292a2ed8a8
Ahmedalmasrii <ahmedalmasri2013@gmail.com>
2024-02-13 22:34:05 +0000

    skapat en controller sida för min contact sida

---
+++ MovieRatingBase/app/Http/Controllers/ContactController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ContactController extends Controller
+{
+    // Visa kontaktformuläret
+    public function create()
+    {
+        return view('contact'); // contact.blade.php
+    }
+
+    // Hantera inskickat formulär
+    public function store(Request $request)
+    {
+        // Validera och processa data här
+        // Exempel: Kontrollerar ifall rätt data är inmatad
+        $validated = $request->validate([
+            'firstname' => 'required',
+            'lastname' => 'required' ,
+            'email' => 'required|email',
+            'message' => 'required',
+        ]);
+
+        // Processa data (t.ex. spara i databasen, skicka e-post, etc.)
+
+        return redirect()->route('contact.create')->with('success', 'Ditt meddelande har skickats.');
+    }
+}
