d329c0c9dc90a64f6e91993656223d625b7f4d65
Joseph Gros <wiley.joseph.gros@gmail.com>
2024-02-16 15:42:49 +0100

    ett till försök

--- MovieRatingBase/vite.config.js
+++ MovieRatingBase/vite.config.js
@@ -7,7 +7,6 @@ export default defineConfig({
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
-                'resources/views/welcome.php',
             ],
             refresh: true,
         }),
