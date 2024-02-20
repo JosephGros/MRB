7dbe14e3b7f0c3627450066527eb060f0f61695c
Joseph Gros <wiley.joseph.gros@gmail.com>
2024-02-20 15:06:57 +0100

    YES!!

--- MovieRatingBase/routes/web.php
+++ MovieRatingBase/routes/web.php
@@ -20,8 +20,8 @@ Route::get('/', function () {
 
 Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
+});
+// ->middleware(['auth', 'verified'])->name('dashboard');
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
