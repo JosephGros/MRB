a1443b79f9b3534137e132e7ec8ca9ffc5edc055
Joseph Gros <wiley.joseph.gros@gmail.com>
2024-02-22 17:19:22 +0100

    test2

--- MovieRatingBase/resources/views/layouts/navigation.blade.php
+++ MovieRatingBase/resources/views/layouts/navigation.blade.php
@@ -14,7 +14,7 @@
                  <!-- Logo -->
                 <div class="ml-[130px] sm:ml-8">
                     <a href="#">
-                         <img class="h-18 w-30 sm:w-40" src="{{ Vite::asset('images/LogoMRB.png') }}" alt="MRB logo">
+                         <img class="h-18 w-30 sm:w-40" src="{{ asset('/images/LogoMRB.png') }}" alt="MRB logo">
                     </a>
                 </div>
 
