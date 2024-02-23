9956bb5b07b1dde3422d360f130bbdb16965362e
Joseph Gros <144314897+JosephGros@users.noreply.github.com>
2024-02-23 23:11:46 +0100

    Test 4

--- MovieRatingBase/app/Http/Controllers/Auth/RegisteredUserController.php
+++ MovieRatingBase/app/Http/Controllers/Auth/RegisteredUserController.php
@@ -34,7 +34,6 @@ class RegisteredUserController extends Controller
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
-            'username' => ['required', 'string'],
         ]);
 
         $user = User::create([
