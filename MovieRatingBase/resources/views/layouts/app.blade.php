4c4cc7463c49dc5c7c26b57efee417322d5655ba
Matilda Källström <matilda.kallstrom@hotmail.se>
2024-02-26 13:51:13 +0100

    index - login

--- MovieRatingBase/resources/views/layouts/app.blade.php
+++ MovieRatingBase/resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'MRB') }}</title>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
@@ -14,22 +14,32 @@
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
-<body class="font-sans antialiased">
-    <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
+<body class="font-sans antialiased bg-sky-950">
+    <div class="min-h-screen">
         @include('layouts.navigation')
 
         <!-- Page Heading -->
         @if (isset($header))
-            <header class="bg-white dark:bg-gray-800 shadow">
-                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+            <header class="bg-sky-700/50">
+                <div class="mt-14">
                     {{ $header }}
                 </div>
             </header>
         @endif
 
+        <!-- If watchlist exist show, if not don't show this list -->
+        @if (isset($watchlistExist))
+            <div class="bg-sky-700 md:mx-20 md:rounded-lg">
+                <div class="mt-14">
+                    {{ $watchlistExist }}
+                </div>
+            </div>
+        @endif
+
         <!-- Page Content -->
-        <main>
-            @yield('content')
+        <main class="md:mx-20 md:rounded-lg">
+            <!-- @yield('content') -->
+            {{ $content }}
         </main>
     </div>
 </body>
