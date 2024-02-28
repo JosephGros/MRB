b054553012daa25c117bbc1c8f1411f1ec120556
Ahmedalmasrii <ahmedalmasri2013@gmail.com>
2024-02-28 00:26:47 +0000

    ändrat text storlek, lagt till padding och margin

--- MovieRatingBase/resources/views/userprofile.blade.php
+++ MovieRatingBase/resources/views/userprofile.blade.php
@@ -2,16 +2,16 @@
 
 @section('content')
 
-<div class="container mx-auto px-4 bg-sky-950 text-white">
+<div class="container mx-auto px-4 bg-sky-950 text-white mt-10">
     <div class="py-8">
 
         <!-- Profil info -->
-        <div class="bg-sky-800 bg-opacity-75 rounded overflow-hidden shadow-lg">
+        <div class="max-w-4xl mx-auto bg-sky-800 bg-opacity-75 rounded overflow-hidden shadow-lg ">
             <div class="flex flex-col md:flex-row justify-between items-center text-center md:text-left p-5">
                 <!-- Vänster sida med bild och Edit-knapp -->
                 <div class="flex-shrink-0 mb-4 md:mb-0 text-center">
-                    <img src="{{ asset('/images/profileimage.png') }}" alt="Profile image" id="profileImage" class="w-20 h-20 sm:w-24 sm:h-24 md:w-32 md:h-32 lg:w-36 lg:h-36 xl:w-40 xl:h-40 rounded-full border-4 border-blue-300 object-cover mb-2 mx-auto">
-                    <button class="bg-blue-500 text-white rounded-full px-2 py-1 text-xs" onclick="document.getElementById('imageInput').click();">Edit</button>
+                    <img src="{{ asset('/images/profileimage.png') }}" alt="Profile image" id="profileImage" class="w-20 h-20 sm:w-24 sm:h-24 md:w-32 md:h-32 lg:w-34 lg:h-34 xl:w-36 xl:h-36 rounded-full border-4 border-blue-300 object-cover mb-2 mx-auto">
+                    <button class="bg-blue-500 text-white rounded-full px-6 py-3 text-base" onclick="document.getElementById('imageInput').click();">Edit</button>
                     
                     <!-- Dolt filinput-element -->
                     <input type="file" id="imageInput" style="display: none;" onchange="previewImage();" accept="image/*">
