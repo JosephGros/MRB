9284b7f39502614d26d08f574c7b07346e2cd50d
Ahmedalmasrii <ahmedalmasri2013@gmail.com>
2024-02-28 02:16:34 +0000

    Skapat en Movie section, whatchlist movie  och skrivit på engelska

--- MovieRatingBase/resources/views/userprofile.blade.php
+++ MovieRatingBase/resources/views/userprofile.blade.php
@@ -5,20 +5,20 @@
 <div class="container mx-auto px-4 bg-sky-950 text-white mt-10">
     <div class="py-8">
 
-        <!-- Profil info -->
+        <!-- Profile info -->
         <div class="max-w-4xl mx-auto bg-sky-800 bg-opacity-75 rounded overflow-hidden shadow-lg ">
             <div class="flex flex-col md:flex-row justify-between items-center text-center md:text-left p-5">
-                <!-- Vänster sida med bild och Edit-knapp -->
+                <!-- Left site with img and edit button -->
                 <div class="flex-shrink-0 mb-4 md:mb-0 text-center">
                     <img src="{{ asset('/images/profileimage.png') }}" alt="Profile image" id="profileImage" class="w-20 h-20 sm:w-24 sm:h-24 md:w-32 md:h-32 lg:w-34 lg:h-34 xl:w-36 xl:h-36 rounded-full border-4 border-blue-300 object-cover mb-2 mx-auto">
                     <x-button-dark class="bg-blue-500 text-white rounded-full px-6 py-3 text-base" onclick="document.getElementById('imageInput').click();">Edit</x-button-dark>
 
-                    
-                    <!-- Dolt filinput-element -->
+
+                    <!-- Hidden input field -->
                     <input type="file" id="imageInput" style="display: none;" onchange="previewImage();" accept="image/*">
                 </div>
-                <!-- Höger sida med detaljer -->
-                <div class="flex-grow text-white"> 
+                <!-- Right side -->
+                <div class="flex-grow text-white">
                     <div class="mb-3">
                         <div class="text-lg font-bold">
                             <h3>Username:</h3>
@@ -33,14 +33,14 @@
                     </div>
                     <div class="mb-3">
                         <div class="text-lg font-bold">
-                           <h3>Member since:</h3>
+                            <h3>Member since:</h3>
                         </div>
                         <p>06 - 02 - 2024</p>
                     </div>
 
-                    <!-- Knappar -->
+                    <!-- Buttons -->
                     <div class="flex flex-wrap justify-center md:justify-start space-x-0 md:space-x-3 mt-3">
-                    <x-button-dark class="px-2 sm:px-4 py-1 sm:py-2 rounded mb-2 md:mb-0">Account settings</x-button-dark>
+                        <x-button-dark class="px-2 sm:px-4 py-1 sm:py-2 rounded mb-2 md:mb-0">Account settings</x-button-dark>
                         <x-button-dark class="px-2 sm:px-4 py-1 sm:py-2 rounded mb-2 md:mb-0">New list +</x-button-dark>
                         <x-button-dark class="px-2 sm:px-4 py-1 sm:py-2 rounded mb-2 md:mb-0">Delete account</x-button-dark>
                         <x-button-dark class="px-2 sm:px-4 py-1 sm:py-2 rounded mb-2 md:mb-0">Logout</x-button-dark>
@@ -51,27 +51,71 @@
         </div>
 
     </div>
+
+
     <!--Movie section-->
-   
+
+    <div class="container mx-auto px-4 py-8">
+        <div class="grid grid-cols-1 gap-8">
+            <!-- Watchlist Section -->
+            
+            <div>
+                <h2 class="text-2xl font-bold mb-4">Watchlist</h2>
+                <div class="flex flex-row flex-wrap justify-start items-center gap-4 overflow-x-auto">
+                    <div class="w-1/6 flex-none">
+                        <img src="{{ asset('/images/lotr.jpg') }}" alt="Lord of the rings - The return of the king" class="rounded-lg w-full">
+                        <span class="block text-center mt-2">Lord of the rings</span>
+                    </div>
+
+                    <div class="px-2 w-1/6">
+                        <img src="{{ asset('/images/thg.jpg') }}" alt="The Hunger Games - The Ballad of Songbirds and Snakes" class="rounded-lg w-full">
+                        <span class="block text-center mt-2">The Hunger Games</span>
+                    </div>
+
+                    <div class="px-2 w-1/6">
+                        <img src="{{ asset('/images/oppenheimer.jpg') }}" alt="Oppenheimer" class="rounded-lg w-full">
+                        <span class="block text-center mt-2">Oppenheimer</span>
+                    </div>
+
+                    <div class="px-2 w-1/6">
+                        <img src="{{ asset('/images/deadpool.jpg') }}" alt="Deadpool" class="rounded-lg w-full">
+                        <span class="block text-center mt-2">Deadpool</span>
+                    </div>
+
+                    <div class="px-2 w-1/6">
+                        <img src="{{ asset('/images/insideOut.jpg') }}" alt="InsideOut" class="rounded-lg w-full">
+                        <span class="block text-center mt-2">InsideOut</span>
+                    </div>
+                </div>
+            </div>
+
+
+
+        </div>
+    </div>
+
+
+
+
 </div>
 
 
 
 <script>
-function previewImage() {
-    var file = document.getElementById('imageInput').files[0];
-    var reader = new FileReader();
-    
-    reader.onloadend = function() {
-        document.getElementById('profileImage').src = reader.result;
+    function previewImage() {
+        var file = document.getElementById('imageInput').files[0];
+        var reader = new FileReader();
+
+        reader.onloadend = function() {
+            document.getElementById('profileImage').src = reader.result;
+        }
+
+        if (file) {
+            reader.readAsDataURL(file);
+        } else {
+            document.getElementById('profileImage').src = "{{ asset('/images/profileimage.png') }}";
+        }
     }
-    
-    if (file) {
-        reader.readAsDataURL(file);
-    } else {
-        document.getElementById('profileImage').src = "{{ asset('/images/profileimage.png') }}";
-    }
-}
 </script>
 
 @endsection
