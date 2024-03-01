efd0b71e4812b067d895923b9cf4427e0ec8b368
Matilda Källström <matilda.kallstrom@hotmail.se>
2024-03-01 19:06:22 +0100

    coment out row 103-107

--- MovieRatingBase/app/Http/Controllers/MovieController.php
+++ MovieRatingBase/app/Http/Controllers/MovieController.php
@@ -100,11 +100,11 @@ class MovieController extends Controller
 
             // $movie->writers()->attach($validated['writers']);  
 
-            return redirect()->view('admin.edit.editMovie')->with('success', 'Movie created successfully'); // Behöver ändras när vi har en sida som den ska redirect till!
-        } else {
-            return redirect()->back()->with('error', 'Something went wrong');
-        }
-    }
+    //         return redirect()->view('admin.edit.editMovie')->with('success', 'Movie created successfully'); // Behöver ändras när vi har en sida som den ska redirect till!
+    //     } else {
+    //         return redirect()->back()->with('error', 'Something went wrong');
+    //     }
+    // }
 
     public function show(int $id)
 {
