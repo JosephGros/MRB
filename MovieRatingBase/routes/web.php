478a975c29e41c1051fc1745eee9b69b456d1708
Matilda Källström <matilda.kallstrom@hotmail.se>
2024-03-02 13:43:39 +0100

    removed route /actors-> name(actor.show), notised that we had one

--- MovieRatingBase/routes/web.php
+++ MovieRatingBase/routes/web.php
@@ -236,10 +236,6 @@ Route::get('/contact', [ContactController::class, 'index'])->name('contact.index
 Route::get('/about-us', [App\Http\Controllers\AboutUsController::class, 'index'])->name('about-us');
 
 
-// view for Actors
-Route::get('/actors', [ActorController::class, 'show'])->name('actor.show');
-
-
 //User profile blade route
 
 // Route::get('/user-profile', [UserProfileIndexController::class, 'show'])->name('user.profile.show');
